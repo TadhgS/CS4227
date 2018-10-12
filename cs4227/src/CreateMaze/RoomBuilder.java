d86ca822900c91319067c37ebeb827b20ad177e7
14138409 <14138409@CS304-30>
2018-10-12 10:37:49 +0100

    created new branch

---
+++ cs4227/src/CreateMaze/RoomBuilder.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CreateMaze;
+
+/**
+ *
+ * @author 14138409
+ */
+public class RoomBuilder {
+    
+}
