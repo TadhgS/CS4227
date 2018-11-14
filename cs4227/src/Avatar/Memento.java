d63251a6bff30aabfe035fea26d17558f2c49a8a
Tadhg Sheehan <14138808@studentmail.ul.ie>
2018-11-14 12:24:54 +0000

    Memento created

--- cs4227/src/Avatar/Memento.java
+++ cs4227/src/Avatar/Memento.java
@@ -1,18 +1,30 @@
 
 package Avatar;
+import CreateMaze.Room;
 
 class Memento 
 {
     private int Hp;
+    private Room room;
     
     public Memento(int hp)
     {
         this.Hp = hp;   
     }
     
+    public Memento(Room room)
+    {
+        this.room = room;   
+    }
+    
     public int getHp()
     {
         return Hp;
+    }
+    
+    public Room getRoom()
+    {
+        return room;
     }
     
 }
