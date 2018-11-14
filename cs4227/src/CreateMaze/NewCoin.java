3f65cacaedaff5f9f5beed6ab920c50a8d226de1
Tadhg Sheehan <14138808@studentmail.ul.ie>
2018-11-14 12:59:34 +0000

    Memento created

--- cs4227/src/CreateMaze/NewCoin.java
+++ cs4227/src/CreateMaze/NewCoin.java
@@ -1,16 +1,15 @@
 package CreateMaze;
 import Avatar.Avatar;
-/**
- *
- * @author Tadhg
- */
-/*public class NewCoin implements CoinInterface
+
+ 
+public class NewCoin implements CoinInterface
 {
-    @Override
+   @Override
    public void updateScore(Avatar a)
    {
        int score = 10;
        a.setScore(score);
    }
+
     
-}*/
+}
