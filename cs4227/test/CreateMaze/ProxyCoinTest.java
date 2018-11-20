5122a926f71bd2e7eb648e2b241498d4b1e70efc
dkeating21 <danielpkeating21@gmail.com>
2018-11-20 11:35:07 +0000

    Removed comments

--- cs4227/test/CreateMaze/ProxyCoinTest.java
+++ cs4227/test/CreateMaze/ProxyCoinTest.java
@@ -1,97 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CreateMaze;
 
 import Avatar.Avatar;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author User
- */
-public class ProxyCoinTest {
-    
-    public ProxyCoinTest() {
+public class ProxyCoinTest 
+{   
+    public ProxyCoinTest() 
+    {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of updateScore method, of class ProxyCoin.
      */
     @Test
-    public void testUpdateScore1() {
+    public void testUpdateScore1() 
+    {
         System.out.println("updateScore");
         Avatar a = new Avatar();
         ProxyCoin instance = new ProxyCoin();
         
         int num = 3;
         a.setScore(num);
-        for(int i = 0; i < a.getScore(); i++){
+        for(int i = 0; i < a.getScore(); i++)
+        {
             instance.updateScore(a);    
         }
         assertEquals(30, a.getPoints());
     }
     
     @Test
-    public void testUpdateScore2() {
+    public void testUpdateScore2() 
+    {
         System.out.println("updateScore");
         Avatar a = new Avatar();
         ProxyCoin instance = new ProxyCoin();
         
         int num = 10;
         a.setScore(num);
-        for(int i = 0; i < a.getScore(); i++){
+        for(int i = 0; i < a.getScore(); i++)
+        {
             instance.updateScore(a);    
         }
         assertEquals(100, a.getPoints());
     }
         
     @Test
-    public void testUpdateScore3() {
+    public void testUpdateScore3() 
+    {
         System.out.println("updateScore");
         Avatar a = new Avatar();
         ProxyCoin instance = new ProxyCoin();
         
         int num = 67;
         a.setScore(num);
-        for(int i = 0; i < a.getScore(); i++){
+        for(int i = 0; i < a.getScore(); i++)
+        {
             instance.updateScore(a);    
         }
         assertEquals(670, a.getPoints());
     }
         
     @Test
-    public void testUpdateScore4() {
+    public void testUpdateScore4() 
+    {
         System.out.println("updateScore");
         Avatar a = new Avatar();
         ProxyCoin instance = new ProxyCoin();
         
         int num = -42;
         a.setScore(num);
-        for(int i = 0; i < a.getScore(); i++){
+        for(int i = 0; i < a.getScore(); i++)
+        {
             instance.updateScore(a);    
         }
         assertEquals(0, a.getPoints());
